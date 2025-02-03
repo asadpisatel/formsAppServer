f18c755fac232f0147d12754cb87fc02d50a2902
asadpisatel <berdibaev.asadbek@gmail.com>
2025-02-03 21:08:37 +0500

    added logic for login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { hashSync } = require("bcrypt");
+const { hashSync, compareSync } = require("bcrypt");
 const salt = 10;
 
 exports.createUser = async (req, res) => {
   try {
     if (await prisma.user.findUnique({ where: { email: req.body.email } })) {
-      return res.status(409).json({ error: `Email is already registered` });
+      return res.status(409).json({ error: "Email is already registered" });
     }
     const newUser = await prisma.user.create({
       data: {
@@ -16,6 +16,25 @@ exports.createUser = async (req, res) => {
     });
 
     return res.status(201).json(newUser);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { email: req.body.email },
+  });
+  try {
+    if (!user) {
+      return res.status(401).json({ error: "Email not registered" });
+    }
+
+    if (!compareSync(req.body.password, user.password)) {
+      return res.status(403).json({ error: "Incorrect password" });
+    }
+
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 
 router.post("/registration", userController.createUser);
+router.post("/login", userController.loginUser);
 
 module.exports = router;
