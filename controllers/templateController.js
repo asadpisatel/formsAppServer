2555efcbc3c74a209bdea431489243a7b1b0873a
asadpisatel <berdibaev.asadbek@gmail.com>
2025-02-17 15:48:44 +0500

    added apis for getting and update  values of template

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -17,10 +17,44 @@ exports.checkTemplate = async (req, res) => {
       },
     });
 
-    res.json({
+    return res.json({
       isAuthor: template.userId === userId,
       hasResponse: template.responses.length > 0,
     });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getTemplate = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const template = await prisma.template.findUnique({
+      where: { id },
+      select: {
+        title: true,
+        description: true,
+        topic: true,
+      },
+    });
+
+    return res.status(200).json(template);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateTemplate = async (req, res) => {
+  try {
+    const { id, title, description, topic } = req.body;
+
+    const updatedTemplate = await prisma.template.update({
+      where: { id },
+      data: { title, description, topic },
+    });
+
+    return res.status(200).json({ template: updatedTemplate });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- routes/templateRoute.js
+++ routes/templateRoute.js
@@ -3,5 +3,7 @@ const { authMiddleware } = require("../middleware/middleware");
 const templateController = require("../controllers/templateController");
 
 router.post("/check", authMiddleware, templateController.checkTemplate);
+router.get("/:id", authMiddleware, templateController.getTemplate);
+router.put("/update", authMiddleware, templateController.updateTemplate);
 
 module.exports = router;
